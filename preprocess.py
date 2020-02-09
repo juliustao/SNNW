91c2252146ce648cc3f1de522a050bed013bde87
juliustao <juliusltao@gmail.com>
2020-02-09 13:28:08 -0500

    get num labels automatically

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,16 @@ from PIL import Image
 import numpy as np
 
 
-def png2np(paths):
+def save_png_as_np(split):
+    with open(split + '.txt', 'r') as f:
+        paths = f.read().splitlines()
+    # number of labels
+    labels = 0
+    # doesn't matter what path we use to get the split directory name
+    split_dir = os.path.basename(os.path.dirname(os.path.dirname(paths[0])))
+    for _, dirnames, _ in os.walk(split_dir):
+        labels += len(dirnames)
+
     np_images = []
     np_labels = []
     for path in tqdm(paths):
@@ -14,24 +23,18 @@ def png2np(paths):
 
         # get label from folder name of file
         label = int(os.path.basename(os.path.dirname(path)))
-        # np_label shape: [1 x 10]
-        np_label = np.zeros(shape=(1, 10))
+
+        # np_label shape: [1 x #labels]
+        np_label = np.zeros(shape=(1, labels))
         np_label[0, label] = 1.0
         np_labels.append(np_label)
-    return np.concatenate(np_images), np.concatenate(np_labels)
-
-
-def save_png_as_np(split):
-    with open(split + '.txt', 'r') as f:
-        paths = f.read().splitlines()
-    np_images, np_labels = png2np(paths)
     np.save(split + '-images.npy', np_images)
     np.save(split + '-labels.npy', np_labels)
 
 
 if __name__ == '__main__':
     # assumes that train.txt and test.txt are in the same directory
-    splits = ['train', 'test']
+    splits = ['test']
     for split in splits:
         print('Current split: ' + split)
         save_png_as_np(split)
