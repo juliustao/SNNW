e19a30e8475ac89d40e0a9dd1ad09b34c83d3d4a
juliustao <juliusltao@gmail.com>
2020-02-09 13:12:16 -0500

    convert PNG to numpy arrays

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,37 @@
+import os
+from tqdm import tqdm
 from PIL import Image
 import numpy as np
 
 
 def png2np(paths):
-    np_arrs = []
-    for path in paths:
-        # np_arr shape: [1 x 784]
-        np_arr = np.expand_dims(np.array(Image.open(path)).flatten(), 0)
-        np_arrs.append(np_arr)
-    return np.concatenate(np_arrs)
+    np_images = []
+    np_labels = []
+    for path in tqdm(paths):
+        # np_image shape: [1 x 784]
+        np_image = np.expand_dims(np.array(Image.open(path)).flatten(), 0)
+        np_images.append(np_image)
+
+        # get label from folder name of file
+        label = int(os.path.basename(os.path.dirname(path)))
+        # np_label shape: [1 x 10]
+        np_label = np.zeros(shape=(1, 10))
+        np_label[0, label] = 1.0
+        np_labels.append(np_label)
+    return np.concatenate(np_images), np.concatenate(np_labels)
 
 
 def save_png_as_np(split):
-    paths = []
     with open(split + '.txt', 'r') as f:
-        paths.append(f.readline())
-    np_arr = png2np(paths)
-    np.save(split + '.npy', np_arr)
+        paths = f.read().splitlines()
+    np_images, np_labels = png2np(paths)
+    np.save(split + '-images.npy', np_images)
+    np.save(split + '-labels.npy', np_labels)
 
 
 if __name__ == '__main__':
+    # assumes that train.txt and test.txt are in the same directory
     splits = ['train', 'test']
     for split in splits:
+        print('Current split: ' + split)
         save_png_as_np(split)
