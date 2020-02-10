b94c47f071a2186ba1599a3fb1ba6d5caa50e43e
juliustao <juliusltao@gmail.com>
2020-02-09 19:01:59 -0500

    change variable names

--- losses/CrossEntropy.py
+++ losses/CrossEntropy.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def function(y_pred, y_true):
+def function(y_pred, y_gt):
     # shape y_pred [1 x _]
     # y_true is [1 x _]
     assert (abs(np.sum(y_pred) - 1.0) < 1e-6), 'y_pred fed into CrossEntropy is not a probability array'
     log_likelihood = -np.log(y_pred)
-    loss = np.dot(log_likelihood, y_true) / np.sum(y_true)
+    loss = np.dot(log_likelihood, y_gt) / np.sum(y_gt)
     return loss
 
 
