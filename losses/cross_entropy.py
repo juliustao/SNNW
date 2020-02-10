d6d37ecde7009613ddbe9f65595ab1163e9523a8
juliustao <juliusltao@gmail.com>
2020-02-09 22:40:46 -0500

    change equality threshold

--- losses/cross_entropy.py
+++ losses/cross_entropy.py
@@ -4,7 +4,7 @@ import numpy as np
 def function(y_pred, y_gt):
     # shape y_pred [1 x _]
     # y_true is [1 x _]
-    assert (abs(np.sum(y_pred) - 1.0) < 1e-6), 'y_pred fed into cross entropy is not a probability array'
+    assert (abs(np.sum(y_pred) - 1.0) < 1e-5), 'y_pred fed into cross entropy is not a probability array'
     log_likelihood = -np.log(y_pred)
     loss = np.sum(log_likelihood * y_gt) / np.sum(y_gt)
     return loss
